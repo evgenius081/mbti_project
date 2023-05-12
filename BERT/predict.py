99be782e6ffabdebd138752ab88d7af73a433962
evgenius081 <gulevichzhenya@gmail.com>
2023-05-12 22:49:05 +0200

    add: added project files

---
+++ BERT/predict.py
@@ -0,0 +1,27 @@
+from transformers import pipeline
+from datasets import ClassLabel
+
+c2l = ClassLabel(names=[
+                     "INTJ", "INTP", "ENTJ", "ENTP",
+                     "INFJ", "INFP", "ENFJ", "ENFP",
+                     "ISTJ", "ISFJ", "ESTJ", "ESFJ",
+                     "ISTP", "ISFP", "ESTP", "ESFP"])
+
+classificator = pipeline("text-classification", model="BERT/model", tokenizer="distilbert-base-uncased", framework="pt", top_k=16)
+
+def predict_for_texts(texts, verbose = False):
+    # predict
+    predictions = {}
+    batch_size = 512
+    batches = [texts[i:i + batch_size] for i in range(0, len(texts), batch_size)]
+
+    for batch in batches:
+        result = classificator(batch, padding=True)
+        top_results = [(c2l.int2str(int(res['label'].removeprefix("LABEL_"))), res['score']) for res in result[0]]
+        for label, score in top_results:
+            if label not in predictions:
+                predictions[label] = 0
+            predictions[label] += score
+    sorted_tuple = sorted(predictions.items(), key=lambda item: item[1], reverse=True)[0]
+    return sorted_tuple[0]
+
---
+++ Parser.py
@@ -0,0 +1,59 @@
+import praw
+from configparser import ConfigParser
+from pathlib import Path
+
+from prawcore import NotFound
+
+
+class Parser:
+    def __init__(self):
+        config_object = ConfigParser()
+        config_object.read("config.ini")
+        user_info = config_object["AUTH_INFO"]
+        self.reddit = praw.Reddit(
+            client_id=user_info["client_id"],
+            client_secret=user_info["client_secret"],
+            user_agent=user_info["user_agent"],
+        )
+        self.types = ["ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP", "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ"]
+        self.banned_words = ["https://", "[removed]"]
+        self.symbol_mapper = [["*", ""], ["\\", " "], ["|", ""], ["\n", " "], ["#", ""], ["\"", ""], ["  ", " "], 
+                              ["'", ""]]
+
+    def get_users_texts(self, author):
+        try:
+            return self.get_user_comments(author) + self.get_user_posts(author)
+        except Exception as e:
+             pass
+
+    def get_user_posts(self, username):
+        res_list = []
+        submissions = self.reddit.redditor(username).submissions.new(limit=100000)
+        for line in submissions:
+            if line.selftext != "":
+                res_list.append(self.map_symbols(line.selftext))
+        return res_list
+
+    def get_user_comments(self, username):
+        res_list = []
+        comments = self.reddit.redditor(username).comments.new(limit=100000)
+        for line in comments:
+            if line.body != "":
+                res_list.append(self.map_symbols(line.body))
+        return res_list
+
+    def map_symbols(self, text):
+        new_text = text
+        for pair in self.symbol_mapper:
+            new_text = new_text.replace(pair[0], pair[1])
+        return new_text
+
+    def user_exists(self, username):
+        try:
+            self.reddit.redditor(username).id
+        except NotFound:
+            return False
+        return True
+
+    def get_equal_dataset(self):
+        pass
